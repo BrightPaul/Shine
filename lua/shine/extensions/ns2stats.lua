482ba6d34bd424d13c8ef16e6a24cbe5c446cc53
BrightPaul <sebi4d@gmx.de>
2013-08-03 19:43:23 +0200

    slowly we are getting somewhere

--- lua/shine/extensions/ns2stats.lua
+++ lua/shine/extensions/ns2stats.lua
@@ -29,17 +29,53 @@ function Plugin:Initialise()
     Shine.Hook.SetupClassHook( "DamageMixin", "DoDamage", "DealedDamage", "PassivePost" )
     Shine.Hook.SetupClassHook{ "ScoringMixin", "AddScore","ScoreChanged","PassivePost")
     Shine.Hook.SetupClassHock( "NS2Gamerules", "OnEntityDestroy","OnPlayerDeath","PassivePost")
+    Shine.Hook.SetupClassHook("ResearchMixin","TechResearched","OnTechResearched","PassivePost")
+    Shine.Hook.SetupClassHook("ResearchMixin","SetResearching""OnTechStartResearch","PassivePre")
     //Todo: Add all add functions + sendtoserver
     // add all Data Function to Hooks Shine.Hook.Add( string HookName, string UniqueID, function HookFunction [, int Priority ] )
     Shine.Hook.Add( "BuildingDropped", "AddBuildingdropped", function(newEnt, commander) /*add function stuff here*/ end )
     Shine.Hook.Add( "DealedDamage", "AddDamagetoS", function(target,attacker,damage)
-    /*if attacker:isa("Player") then
-    local id = self.score[toString(attacker]
-    if damage > 0  then 
-        if target:isa("Player") then
-        self.assist[toString(target:GetUserId())][toString(attacker:GetUserId())]= true
-           //hit_player
-    end end*/ end )
+    if attacker:isa("Player") then
+        if damage > 0  then 
+        local hit = {} 
+        local atloc = attacker:GetOrigin()
+        hit.attacker_steamId = attacker:GetUserId()
+        hit.attacker_team = attacker:GetTeamNumber()
+        hit.attacker_weapon = attacker:GetActiveWeaponName()
+        hit.attacker_hp = attacker:GetHealth()
+        hit.attacker_amor = attacker:GetArmor()
+        hit.attackerx = atloc.x
+        hit.attackery = atloc.y
+        hit.attaclerz = atloc.z
+            //Player
+            if target:isa("Player") then
+                local tarloc target:GetOrigin()
+                hit.action = "hit_player"
+                hit.target_steamId = target:GetUserId()
+                hit.target_team = target:GetTeamNumber()
+                hit.target_weapon = target:GetActiveWeaponName()
+                hit.target_lifeform = target:GetMapName()
+                target_hp = target:GetHealth()
+                target_armor = target:GetArmor(),
+                targetx = toString(targetloc.x) ,
+                targety = toString(targetloc.y) ,
+                targetz = toString(targetloc.z),    
+            else target:isa("Structure")
+                local strloc = target:GetOrigin()
+                hit.structure_id = target:GetID(),
+                hit.structure_team = target:GetTeamNumber()
+                hit.structure_cost= target:GetCost()
+                hit.structure_name = target:GetMapName()
+                hit.structure_x = toString(strloc.x)
+                hit.structure_y = toString(strloc.y)
+                hit.structure_z = toString(strloc.z)  
+                hit.action = "hit_structure"
+            end
+        self:addLog(hit)
+        else then //Miss
+        end
+     end*/ end )
+    
     Shine.Hook.Add("OnFinishedBuilt","AddBuildtoStats",function(builder) 
         local strloc = self:GetOrigin()
         local build=
@@ -49,13 +85,15 @@ function Plugin:Initialise()
             steamId = builder:GetUserId()
             structure_cost=self:GetCost()
             team = builder:GetTeamNumber(),
-            structure_name = TargetEntity.kMapName,
+            structure_name = self:GetMapName(),
             structure_x = toString(strloc.x),
             structure_y = toString(strloc.y),
             structure_z = toString(strloc.z)
         }
-        addLog(build)
+        self:addLog(build)
         end)
+    Shine.Hook.Add("OnTechResearched","AddStatFTech", self:OnUpgradeFinished(structure, researchId))
+    Shine.Hook.Add("OnTechStartResearch","AddStatSTech", self:addUpgradeStartedToLog(researchNode, player)
 end
 
 function Plugin:OnEntityKilled(Gamerules, TargetEntity, Attacker, Inflictor, Point, Direction)
@@ -67,7 +105,7 @@ function Plugin:OnEntityKilled(Gamerules, TargetEntity, Attacker, Inflictor, Poi
             id = TargetEntity:GetID(),
             structure_team = TargetEntity:GetTeamNumber(),
             structure_cost= TargetEntity:GetCost(),
-            structure_name = TargetEntity.kMapName,
+            structure_name = TargetEntity:GetMapName(),
             structure_x = toString(strloc.x),
             structure_y = toString(strloc.y),
             structure_z = toString(strloc.z)        
@@ -76,7 +114,7 @@ function Plugin:OnEntityKilled(Gamerules, TargetEntity, Attacker, Inflictor, Poi
             death.action = "structure_suicide"
         else
             death.killerid = Attacker:GetUserId() 
-            death.killer_lifeform = Attacker.kMapName
+            death.killer_lifeform = Attacker:GetMapName()
             death.killer_team = Attacker:GetTeamNumber()
         end       
         
@@ -89,7 +127,7 @@ function Plugin:OnEntityKilled(Gamerules, TargetEntity, Attacker, Inflictor, Poi
             target_steamId = TargetEntity:GetUserId(),
             target_team = TargetEntity:GetTeamNumber(),
             target_weapon = TargetEntity:GetActiveWeaponName(),
-            target_lifeform = TargetEntity.kMapName,
+            target_lifeform = TargetEntity:GetMapName(),
             //Why this values? Dead ppl alive?
             target_hp = 0,
             target_armor = 0,
@@ -104,21 +142,56 @@ function Plugin:OnEntityKilled(Gamerules, TargetEntity, Attacker, Inflictor, Poi
         elseif TargetEntity == nil then
             death.attacker_weapon = "natural cause"
         elseif Attacker:isa("Player")
-            local atloc = attacker:GetOrigin()
+            local atloc = Attacker:GetOrigin()
             death.attacker_steamId = Attacker:GetUserId()
             death.attacker_team = Attacker:GetTeamNumber()
             death.attacker_weapon = Attacker:GetActiveWeaponName()
             death.attacker_hp = Attacker:GetHealth()
-            death.attacker_amor = Attacker::GetArmor()
+            death.attacker_amor = Attacker:GetArmor()
             death.attackerx = atloc.x
             death.attackery = atloc.y
             death.attaclerz = atloc.z
         //Whips etc here.
         end
     end
-    addLog(death)
+    self:addLog(death)
+end
+
+function Plugin:addUpgradeStartedToLog(researchNode, player)
+    if player:isa("Commander") then
+        local client = Server.GetOwner(commander)
+        local steamId = ""
+        if client ~= nil then steamId = client:GetUserId() end
+        local techId = researchNode:GetTechId()
+
+        local newUpgrade =
+        {
+        structure_id = researchnode:GetId(),
+        commander_steamid = steamId,
+        team = player:GetTeamNumber(),
+        cost = GetCostForTech(techId),
+        upgrade_name = EnumToString(kTechId, techId),
+        action = "upgrade_started"
+        }
+
+        self:addLog(newUpgrade)
     end
-function OnGamestart()
+
+end
+
+function Plugin:OnUpgradeFinished(structure, researchId)
+    local upgrade =
+    {
+        structure_id = structure:GetId(),
+        team = structure:GetTeamNumber(),
+        commander_steamid = -1,
+        upgrade_name = EnumToString(kTechId, researchId),
+        costs = GetCostForTech(researchId),
+        action ="upgrade_finished"    
+    }
+    self:addLog(upgade)
+end
+function Plugin:OnGamestart()
     //Player Start_list
 end
     //Round ends
@@ -134,9 +207,9 @@ function Plugin:ClientConnect( Client )
     if not Player then return end
     local connect={
             action = "connect"
-            steamId = Client:GetUserId()
+            steamId = Player:GetUserId()
     }
-    addLog(connect)   
+    self:addLog(connect)   
 end
 //PlayerDisconnect
 function Plugin:ClientDisconnect(Client)
@@ -146,10 +219,10 @@ function Plugin:ClientDisconnect(Client)
     if not Player then return end
     local connect={
             action = "disconnect"
-            steamId = Client:GetUserId()
-            score = self.score[toString(Client:GetUserId())].score
+            steamId = Player:GetUserId()
+            score = Player.score
     }
-    addLog(connect)
+    self:addLog(connect)
 end
 // Player joins a team
 function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force )
@@ -160,23 +233,23 @@ function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force )
         team = Player:GetTeamNumber(),
         steamId = Player:GetUserId()
     }
-    addLog(join)
+    self:addLog(join)
 end
 
 //all the send Stuff
 
 self.logInit = false
 self.log = ""
-RBPSlogPartNumber = 0
+RBPSlogPartNumber = 1
 
-function initLog ()
+function Plugin:initLog ()
     self.logInit = true
     self.log = ""
 end
 
-function addLog(tbl)
+function Plugin:addLog(tbl)
     
-    if self.logInit == false then initLog() end
+    if self.logInit == false then self:initLog() end
     
     if tbl == nil then
         return
@@ -187,12 +260,12 @@ function addLog(tbl)
     self.log = self.log .. json.encode(tbl) .."\n"	
     //local data = RBPSlibc:CompressHuffman(RBPSlog)
     //Notify("compress size: " .. string.len(data) .. "decompress size: " .. string.len(RBPSlibc:Decompress(data)))        
-RBPSlogPartNumber = RBPSlogPartNumber + 1
-end
+    RBPSlogPartNumber = RBPSlogPartNumber + 1
+    end
 end
 
 
-function sendData()
+function Plugin:sendData()
  //sendata is only called at gameend,so :
  RBPSgameFinished = 1  
     local params =
@@ -205,124 +278,119 @@ function sendData()
     }
     
     RBPSlastGameFinished = RBPSgameFinished
-       
-  
-
-if RBPSlastLog == nil then
-RBPSlastLogPartNumber = RBPSlogPartNumber	
-RBPSlastLog = self.log
-self.initLog() //clears log	
-    else //if we still have data in last log, we wont send data normally, since it would be duplicated data
-    
-        local totalLength = string.len(RBPSlastLog) + string.len(RBPSlog)
+   if RBPSlastLog == nil then
+    RBPSlastLogPartNumber = RBPSlogPartNumber	
+    RBPSlastLog = self.log
+    self.initLog() //clears log	
+        else //if we still have data in last log, we wont send data normally, since it would be duplicated data
         
-        if totalLength>500000 then //we dont want to have more than 500 000 characters since that seems to crash the server
-            RBPSlastLog = nil //basicly log fails here, but continue anyway
-        else
-            RBPSlastLog = RBPSlastLog .. RBPSlog //save log in memory if we need to resend, keep last log also in memory if send failed	
-        end	
-                        
-self.initLog() //clears log
-//since we do not send log part we dont need to increase part count
-RBPSlogPartNumber = RBPSlogPartNumber - 1 //is increased after this function happens
-return
-end	
-
-Shared.SendHTTPRequest(self.websiteDataUrl, "POST", params, function(response,status) self.onHTTPResponseFromSend(client,"send",response,status) end)	
-
-    RBPSsendStartTime = Shared.GetSystemTime()
-end
-
-
-function resendData()
-     
-    local params =
-    {
-        key = RBPSadvancedConfig.key,
-        roundlog = RBPSlastLog,
-        part_number = RBPSlastLogPartNumber,
-        last_part = RBPSlastGameFinished
-    }
-    
-    if RBPSdebug then
-Notify("Resending part of data to :" .. RBPS.websiteDataUrl)
-end	
-
-Shared.SendHTTPRequest(RBPS.websiteDataUrl, "POST", params, function(response,status) RBPS:onHTTPResponseFromSend(client,"send",response,status) end)	
-
-    RBPSsendStartTime = Shared.GetSystemTime()
-    RBPSresendCount = RBPSresendCount + 1
-end
-
-function onHTTPResponseFromSend(client,action,response,status)	
-    if RBPSdebug and status then
-        Notify("Status: (" .. status.. ")")
-    end
-    local message = json.decode(response)
-    if RBPSdebug then
-        Notify("Sending part of round data completed (" .. (Shared.GetSystemTime() - RBPSsendStartTime) .. " seconds)")
-    end
-    
-    if message then
-    
-        if string.len(response)>0 then //if we got somedata, that means send was completed
-            RBPSlastLog = nil
-            RBPSsuccessfulSends = RBPSsuccessfulSends +1
-        end
-    
-        if message.other then
-RBPS:messageAll(message.other)
-end
-
-        if RBPSdebug then
-Notify(json.encode(response))	
-end
-    
-if message.error == "NOT_ENOUGH_PLAYERS" then
-               local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
-           /* for p = 1, #playerList do
+            local totalLength = string.len(RBPSlastLog) + string.len(RBPSlog)
             
-                
-                Cout:SendMessageToClient(playerList[p], "lastRoundNotEnoughPlayers",{lastRound = nil})
-                  
-            end */
-            return
-        end	
+            if totalLength>500000 then //we dont want to have more than 500 000 characters since that seems to crash the server
+                RBPSlastLog = nil //basicly log fails here, but continue anyway
+            else
+                RBPSlastLog = RBPSlastLog .. RBPSlog //save log in memory if we need to resend, keep last log also in memory if send failed	
+            end	
+                            
+    self.initLog() //clears log
+    //since we do not send log part we dont need to increase part count
+    RBPSlogPartNumber = RBPSlogPartNumber - 1 //is increased after this function happens
+    return
+    end	
 
-if message.link then	
-            local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
-            /*for p = 1, #playerList do
-                
-                Cout:SendMessageToClient(playerList[p], "lastRoundLink",{lastRound = self.websiteUrl .. message.link})
-                  
-            end*/
-end	
-    elseif response then //if message = nil, json parse failed prob or timeout
-        if string.len(response)>0 then //if we got somedata, that means send was completed
-            RBPSlastLog = nil
-            RBPSsuccessfulSends = RBPSsuccessfulSends +1
+    Shared.SendHTTPRequest(self.websiteDataUrl, "POST", params, function(response,status) self.onHTTPResponseFromSend(client,"send",response,status) end)	
+
+        RBPSsendStartTime = Shared.GetSystemTime()
+    end
+
+
+ function Plugin:resendData()
+         
+        local params =
+        {
+            key = RBPSadvancedConfig.key,
+            roundlog = RBPSlastLog,
+            part_number = RBPSlastLogPartNumber,
+            last_part = RBPSlastGameFinished
+        }
+        
+        if RBPSdebug then
+    Notify("Resending part of data to :" .. RBPS.websiteDataUrl)
+    end	
+
+    Shared.SendHTTPRequest(RBPS.websiteDataUrl, "POST", params, function(response,status) RBPS:onHTTPResponseFromSend(client,"send",response,status) end)	
+
+        RBPSsendStartTime = Shared.GetSystemTime()
+        RBPSresendCount = RBPSresendCount + 1
+    end
+
+ function Plugin:onHTTPResponseFromSend(client,action,response,status)	
+        if RBPSdebug and status then
+            Notify("Status: (" .. status.. ")")
         end
-        Notify("NS2Stats.org: (" .. response .. ")")
-end
+        local message = json.decode(response)
+        if RBPSdebug then
+            Notify("Sending part of round data completed (" .. (Shared.GetSystemTime() - RBPSsendStartTime) .. " seconds)")
+        end
+        
+        if message then
+        
+            if string.len(response)>0 then //if we got somedata, that means send was completed
+                RBPSlastLog = nil
+                RBPSsuccessfulSends = RBPSsuccessfulSends +1
+            end
+        
+            if message.other then
+    RBPS:messageAll(message.other)
+    end
+
+            if RBPSdebug then
+    Notify(json.encode(response))	
+    end
+        
+    if message.error == "NOT_ENOUGH_PLAYERS" then
+                   local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
+               /* for p = 1, #playerList do
+                
+                    
+                    Cout:SendMessageToClient(playerList[p], "lastRoundNotEnoughPlayers",{lastRound = nil})
+                      
+                end */
+                return
+            end	
+
+    if message.link then	
+                local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
+                /*for p = 1, #playerList do
+                    
+                    Cout:SendMessageToClient(playerList[p], "lastRoundLink",{lastRound = self.websiteUrl .. message.link})
+                      
+                end*/
+    end	
+        elseif response then //if message = nil, json parse failed prob or timeout
+            if string.len(response)>0 then //if we got somedata, that means send was completed
+                RBPSlastLog = nil
+                RBPSsuccessfulSends = RBPSsuccessfulSends +1
+            end
+            Notify("NS2Stats.org: (" .. response .. ")")
+    end
 
 end
 
+function Plugin:sendServerStatus(gameState)
+    local stime = Shared.GetGMTString(false)
+    local gameTime = Shared.GetTime() - RBPS.gamestarted
+        local params =
+        {
+            key = RBPSadvancedConfig.key,
+            players = json.encode(RBPS.Players),
+            state = gameState,
+            time = stime,
+            gametime = gameTime,
+            map = Shared.GetMapName(),
+        }
 
-
-function sendServerStatus(gameState)
-local stime = Shared.GetGMTString(false)
-local gameTime = Shared.GetTime() - RBPS.gamestarted
-    local params =
-    {
-        key = RBPSadvancedConfig.key,
-        players = json.encode(RBPS.Players),
-        state = gameState,
-        time = stime,
-        gametime = gameTime,
-        map = Shared.GetMapName(),
-    }
-
-Shared.SendHTTPRequest(RBPS.websiteStatusUrl, "POST", params, function(response,status) RBPS:onHTTPResponseFromSendStatus(client,"sendstatus",response,status) end)	
+    Shared.SendHTTPRequest(RBPS.websiteStatusUrl, "POST", params, function(response,status) RBPS:onHTTPResponseFromSendStatus(client,"sendstatus",response,status) end)	
 
 end
 
